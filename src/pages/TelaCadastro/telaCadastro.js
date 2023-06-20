2fdfc962b951b6f1a2e61dd0fad02ff90bb28f7c
BrayanHubas <118532389+miuka22@users.noreply.github.com>
2023-06-20 12:37:12 -0300

    update

--- src/pages/TelaCadastro/telaCadastro.js
+++ src/pages/TelaCadastro/telaCadastro.js
@@ -7,59 +7,86 @@ function TelaCadastro() {
   const [repitirSenha,setRepitirSenha]= useState ('')
 
   return (
-    <View style={styles.Conteiner}>
-    <View style={styles.ConteinerText}>
-        <Text style={styles.textTitle}>Cadastra-se!</Text>
-        <Text style={styles.textSecu}>Crie sua conta para acessar o aplicativo</Text>
-</View>
-    <View style={styles.textForm}>
-          <TextInput
-          style={styles.TextInput}
-          placeholder=''
-          onChange={text=>
-          setEmail(text)}
-          Value={email}/>
-          
-  </View>
 
-    
+      <View style={styles.Conteiner}>
+      <View style={styles.barraSuperior}>
+        <Text style={styles.textTitle}>Cadastra-se!</Text>
+        <Text style={styles.textSecu}>Crie sua conta para acessar o aplicativo </Text>
+
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.formTxt}>Nome do usuario</Text>
+       <TextInput
+       style={styles.TextInput}
+       placeholder='nome do usuario'
+       onChange={text=>
+        setnameUsuario(nameUsuario)}
+       />
+          <Text style={styles.formTxt}>E-mail</Text>
+       <TextInput
+       style={styles.TextInput}
+       placeholder='Email'
+       onChange={text=>
+        setEmail(email)}
+        />
+          <Text style={styles.formTxt}>Senha</Text>
+       <TextInput
+       style={styles.TextInput}
+       placeholder='Senha'
+       onChange={text=>
+        setSenha(senha)}
+        />
+           <Text style={styles.formTxt}>Confirme a senha</Text>
+       <TextInput
+       style={styles.TextInput}
+       placeholder='Confirme a senha'
+       onChange={text=>
+        setRepitirSenha(repitirSenha)}
+        />
+          
+      </View>
+      </View>
     
         
-  </View>
   );
 }
 const styles = StyleSheet.create({
 Conteiner:{
-  flex:1,
-  backgroundColor:'#423880',
-},
-ConteinerText:{
-  height:100,
-  width:600,
-  backgroundColor:'#BBB2F0',
-  paddign: 10,
-  marginVertical:11,
-  paddingHorizontal:10,
-  
+  flex:2,
+  alignContent:'center',
+  justifyContent:'center',
+  backgroundColor:'#423880'
 
+},
+barraSuperior:{
+  backgroundColor:'#BBB2F0',
+  height: 100,
+  padding:10
 },
 textTitle:{
-  color:'#1A1633',
-  ///fontStyle: 'normal',
-  //fontWeight: 700,
-  fontSize:30,
-  //marginVertical:20,
-  paddingVertical:2,
+
 },
-textSecu:{
-  color:'#1A1633',
-  paddingVertical:5,
-  fontSize:18
+form:{
+  alignItems:'center',
+  justifyContent:'center',
+  marginTop:15,
+  paddingHorizontal:12
+},
+formTxt:{
+  width: 350,
+  padding:5,
+  paddingHorizontal:12
 },
 TextInput:{
-  backgroundColor:'#fff',
-  height:50
-}
+  width: 350,
+  height: 50,
+  fontSize: 16,
+  backgroundColor: '#BAB2ED',
+  marginVertical: 5,
+  borderRadius: 11,
+  padding: 12,
+  alignContent:'center',
 
+}
 })
 export default TelaCadastro;
